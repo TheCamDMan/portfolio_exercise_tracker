61c4e6abd0d00ecebc57142d29952d7777dc6ee6
TheCamDMan <thecamdman@gmail.com>
2021-12-11 09:29:52 -0800

    Delete Dropdown cleanup

--- exercise-ui/src/pages/EditExercisePage.js
+++ exercise-ui/src/pages/EditExercisePage.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useHistory } from "react-router-dom";
-import Dropdown from "../components/Dropdown";
 
 export const EditExercisePage = ({ exerciseToEdit }) => {
     const [name, setName] = useState(exerciseToEdit.name);
